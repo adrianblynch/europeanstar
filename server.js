64d4ac3622a54997b0b1541cb4f73b823fb66964
Adrian Lynch <adi_ady_ade@hotmail.com>
2019-04-03 15:06:57 +0100

    Default to PORT env variable

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(
     maxage: 31536000000, // Cache everything...
     setHeaders: (res, path) => {
       if (path.endsWith("index.html")) {
-        res.setHeader('cache-control', 'max-age=0') // ... Except for the index page
+        res.setHeader("cache-control", "max-age=0") // ... Except for the index page
       }
     }
   })
 )
 
-app.listen(3001)
+app.listen(process.env.PORT || 3000)
