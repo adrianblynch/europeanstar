342f4b0bbb6e1bb1873e647e38ee1671715d1b4d
Adrian Lynch <adi_ady_ade@hotmail.com>
2019-04-07 11:45:21 +0100

    Mock matchMedia for testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
-it("renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe("App component", () => {
+
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
