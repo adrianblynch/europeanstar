a6b5f48f16ca8cba73d793b8ae9a9d757bb39e26
Adrian Lynch <adi_ady_ade@hotmail.com>
2019-06-03 17:27:08 +0100

    Remove old compression script

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,39 +1,58 @@
 const fs = require("fs")
 const zlib = require("zlib")
 const ls = require("directory-tree")
+const chalk = require("chalk")
 
 const getTree = (base, exts = "js|css|html|json") => ls("build", { extensions: new RegExp(`\.(${exts})$`) })
+const getFiles = node =>
+  node.children.flatMap(child =>
+    child.type === "directory" ? getFiles(child) : { originalPath: child.path, originalSize: child.size }
+  )
+const logFiles = files => {
+  files.map(file =>
+    console.log(
+      chalk`${file.originalPath} - {red original: ${file.originalSize}b} {blue gzip: ${
+        file.gzipSize
+      }b} {yellow brotli: ${file.brotliSize}b}`
+    )
+  )
+}
 
-const getPaths = node => node.children.flatMap(child => (child.type === "directory" ? getPaths(child) : child.path))
+const root = getTree("build")
+const files = getFiles(root)
 
-const logPaths = (label, paths) => console.log(`${label}:\n\t${paths.join("\n\t")}`)
+const gzip = file => {
+  return new Promise(resolve => {
+    const gzipPath = `${file.originalPath}.gz`
 
-const uncompressedTree = getTree("build")
-const uncompressedPaths = getPaths(uncompressedTree)
+    fs.createReadStream(file.originalPath)
+      .pipe(zlib.createGzip())
+      .pipe(fs.createWriteStream(gzipPath))
+      .on("close", () => {
+        file.gzipPath = gzipPath
+        file.gzipSize = fs.statSync(gzipPath).size
+        resolve(file)
+      })
+      .on("error", () => reject(`Failed to gzip ${file.originalPath}`))
+  })
+}
 
-logPaths("Compressing", uncompressedPaths)
+const brotli = file => {
+  return new Promise((resolve, reject) => {
+    const brotliPath = `${file.originalPath}.br`
 
-const compressedPaths = []
-
-uncompressedPaths.forEach(path => {
-  const compressedGzipPath = `${path}.gz`
-
-  fs.createReadStream(path)
-    .pipe(zlib.createGzip())
-    .pipe(fs.createWriteStream(compressedGzipPath))
-
-  compressedPaths.push(compressedGzipPath)
-
-  // Available in Node > 11.13.0 - Userland brotli packages will do the trick too for lesser Nodes
-  if (zlib.createBrotliCompress) {
-    const compressedBrotliPath = `${path}.br`
-
-    fs.createReadStream(path)
+    fs.createReadStream(file.originalPath)
       .pipe(zlib.createBrotliCompress())
-      .pipe(fs.createWriteStream(compressedBrotliPath))
+      .pipe(fs.createWriteStream(brotliPath))
+      .on("close", () => {
+        file.brotliPath = brotliPath
+        file.brotliSize = fs.statSync(brotliPath).size
+        resolve(file)
+      })
+      .on("error", () => reject(`Failed to brotli ${file.originalPath}`))
+  })
+}
 
-    compressedPaths.push(compressedBrotliPath)
-  }
-})
-
-logPaths("Compressed", compressedPaths)
+const compressions = Promise.all(files.map(gzip)).then(files =>
+  Promise.all(files.map(brotli)).then(files => logFiles(files))
+)
